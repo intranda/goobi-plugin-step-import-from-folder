2fbb985e3c9b19b7876541bb713c02c3d37bddb9
Robert Sehr <robert.sehr@intranda.com>
2021-11-22 12:25:48 +0100

    prepared config

--- plugin/src/main/java/de/intranda/goobi/plugins/FolderimportStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/FolderimportStepPlugin.java
@@ -1,5 +1,10 @@
 package de.intranda.goobi.plugins;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,9 +25,14 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.List;
 
+import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
+import org.goobi.beans.Process;
 import org.goobi.beans.Step;
+import org.goobi.production.cli.helper.StringPair;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
 import org.goobi.production.enums.PluginType;
@@ -30,42 +40,77 @@ import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
 
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.Helper;
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
+import ugh.dl.DigitalDocument;
+import ugh.dl.DocStruct;
+import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.dl.MetadataType;
+import ugh.dl.Prefs;
+import ugh.exceptions.UGHException;
 
 @PluginImplementation
 @Log4j2
 public class FolderimportStepPlugin implements IStepPluginVersion2 {
-    
+
     @Getter
     private String title = "intranda_step_folderimport";
     @Getter
     private Step step;
-    @Getter
-    private String value;
-    @Getter 
-    private boolean allowTaskFinishButtons;
-    private String returnPath;
+
+    private Process process;
+    private Prefs prefs;
+
+    // doc type configuration
+    private List<StringPair> prefixList = new ArrayList<>();
+    private String mainType;
+    private List<StringPair> suffixList = new ArrayList<>();
+
+    // root folder for images
+    @Setter // setter for junit test, call it after initialize
+    private String rootFolder;
+
+    private MetadataType titleType;
 
     @Override
     public void initialize(Step step, String returnPath) {
-        this.returnPath = returnPath;
         this.step = step;
-                
+        process = step.getProzess();
+        prefs = process.getRegelsatz().getPreferences();
         // read parameters from correct block in configuration file
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
-        value = myconfig.getString("value", "default value"); 
-        allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("Folderimport step plugin initialized");
+        myconfig.setExpressionEngine(new XPathExpressionEngine());
+        rootFolder = myconfig.getString("/imageFolder");
+
+        List<HierarchicalConfiguration> pl = myconfig.configurationsAt("/prefixType");
+        for (HierarchicalConfiguration hc : pl) {
+            String folderName = hc.getString("@foldername");
+            String doctype = hc.getString("@doctype");
+            prefixList.add(new StringPair(folderName, doctype));
+        }
+
+        mainType = myconfig.getString("mainType");
+
+        List<HierarchicalConfiguration> sl = myconfig.configurationsAt("/suffixType");
+        for (HierarchicalConfiguration hc : sl) {
+            String folderName = hc.getString("@foldername");
+            String doctype = hc.getString("@doctype");
+            suffixList.add(new StringPair(folderName, doctype));
+        }
+        titleType = prefs.getMetadataTypeByName("TitleDocMain");
+
     }
 
     @Override
     public PluginGuiType getPluginGuiType() {
-        return PluginGuiType.FULL;
-        // return PluginGuiType.PART;
-        // return PluginGuiType.PART_AND_FULL;
-        // return PluginGuiType.NONE;
+        return PluginGuiType.NONE;
     }
 
     @Override
@@ -80,14 +125,14 @@ public class FolderimportStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String cancel() {
-        return "/uii" + returnPath;
+        return "";
     }
 
     @Override
     public String finish() {
-        return "/uii" + returnPath;
+        return "";
     }
-    
+
     @Override
     public int getInterfaceVersion() {
         return 0;
@@ -97,7 +142,7 @@ public class FolderimportStepPlugin implements IStepPluginVersion2 {
     public HashMap<String, StepReturnValue> validate() {
         return null;
     }
-    
+
     @Override
     public boolean execute() {
         PluginReturnValue ret = run();
@@ -106,9 +151,74 @@ public class FolderimportStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public PluginReturnValue run() {
+        Fileformat fileformat = null;
+        DigitalDocument dd = null;
+        DocStruct logical = null;
+        DocStruct physical = null;
+        // open metadata file
+        try {
+            fileformat = process.readMetadataFile();
+            dd = fileformat.getDigitalDocument();
+            logical = dd.getLogicalDocStruct();
+            if (logical.getType().isAnchor()) {
+                logical = logical.getAllChildren().get(0);
+            }
+            physical = dd.getPhysicalDocStruct();
+
+        } catch (UGHException | IOException | InterruptedException | SwapException | DAOException e) {
+            log.error(e);
+            Helper.setFehlerMeldung("Metadata not readable");
+            return PluginReturnValue.ERROR;
+        }
+
+        // get main title
+        List<? extends Metadata> titles = logical.getAllMetadataByType(titleType);
+        if (titles.isEmpty()) {
+            log.error("No main title found for {}", process.getId());
+            Helper.setFehlerMeldung("No main title found.");
+            return PluginReturnValue.ERROR;
+        }
+        String mainTitle = titles.get(0).getValue();
+
+        // search for folder
+        Path folder = Paths.get(rootFolder, mainTitle);
+
+        // if folder doesn't exist -> error
+        if (!StorageProvider.getInstance().isDirectory(folder)) {
+            log.error("No folder to import found: {}", folder);
+            Helper.setFehlerMeldung("No folder to import found.");
+            return PluginReturnValue.ERROR;
+        }
+
+        // read content of folder
+
+
+
+        // get correct prefix items from configuration file (like Vorderdeckel, Titelblatt,  Buchrücken)
+
+        // get correct suffix items from configuration file (like Buchrücken, Frabkeil, ...)
+
+        // assign folder to prefix and suffix items
+
+        // order other folder
+
+        // create structure elements
+
+        // create page elements
+
+        // copy/move images
+
+        // save
+
+        //BandSerie/Akte/
+        //Deckblatt
+        //Titelblatt
+        //Vorgang* -> Titel generieren "Protokoll vom " +  Date
+        // PublicationYear + Dating generieren
+
         boolean successfull = true;
         // your logic goes here
-        
+
         log.info("Folderimport step plugin executed");
         if (!successfull) {
             return PluginReturnValue.ERROR;
--- plugin/src/test/java/de/intranda/goobi/plugins/FolderimportPluginTest.java
+++ plugin/src/test/java/de/intranda/goobi/plugins/FolderimportPluginTest.java
@@ -1,16 +1,219 @@
 package de.intranda.goobi.plugins;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.easymock.EasyMock;
+import org.goobi.beans.Process;
+import org.goobi.beans.Project;
+import org.goobi.beans.Ruleset;
+import org.goobi.beans.Step;
+import org.goobi.beans.User;
+import org.goobi.production.enums.PluginReturnValue;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.VariableReplacer;
+import de.sub.goobi.helper.enums.StepStatus;
+import de.sub.goobi.metadaten.MetadatenHelper;
+import de.sub.goobi.persistence.managers.MetadataManager;
+import de.sub.goobi.persistence.managers.ProcessManager;
+import ugh.dl.Fileformat;
+import ugh.dl.Prefs;
+import ugh.fileformats.mets.MetsMods;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ MetadatenHelper.class, VariableReplacer.class, ConfigurationHelper.class, ProcessManager.class, MetadataManager.class,
+    ConfigPlugins.class })
+@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*", "jdk.internal.reflect.*" })
 public class FolderimportPluginTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    private String resourcesFolder;
+    private File processDirectory;
+    private File metadataDirectory;
+    private Process process;
+    private Prefs prefs;
+
     @Test
-    public void testVersion() throws IOException {
-        String s = "xyz";
-        assertNotNull(s);
+    public void testInitialize() throws IOException {
+        FolderimportStepPlugin plugin = new FolderimportStepPlugin();
+        assertNotNull(plugin);
+        Step step = process.getSchritte().get(0);
+        plugin.initialize(step, "");
+        assertEquals(plugin.getStep().getTitel(), step.getTitel());
     }
+
+    @Test
+    public void testRunPlugin() throws Exception {
+        FolderimportStepPlugin plugin = new FolderimportStepPlugin();
+        assertNotNull(plugin);
+        Step step = process.getSchritte().get(0);
+        plugin.initialize(step, "");
+        assertEquals(plugin.getStep().getTitel(), step.getTitel());
+
+        plugin.setRootFolder(resourcesFolder + "ImportFolder");
+
+
+        PluginReturnValue response = plugin.run();
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // prepare folder
+        resourcesFolder = "src/test/resources/"; // for junit tests in eclipse
+
+        if (!Files.exists(Paths.get(resourcesFolder))) {
+            resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
+        }
+
+        metadataDirectory = folder.newFolder("metadata");
+
+        processDirectory = new File(metadataDirectory + File.separator + "1");
+        processDirectory.mkdirs();
+        String metadataDirectoryName = metadataDirectory.getAbsolutePath() + File.separator;
+
+        // copy meta.xml
+        Path metaSource = Paths.get(resourcesFolder + "meta.xml");
+        Path metaTarget = Paths.get(processDirectory.getAbsolutePath(), "meta.xml");
+        Files.copy(metaSource, metaTarget);
+        // copy anchor
+        metaSource = Paths.get(resourcesFolder + "meta_anchor.xml");
+        metaTarget = Paths.get(processDirectory.getAbsolutePath(), "meta_anchor.xml");
+        Files.copy(metaSource, metaTarget);
+
+        PowerMock.mockStatic(ConfigurationHelper.class);
+        ConfigurationHelper configurationHelper = EasyMock.createMock(ConfigurationHelper.class);
+        EasyMock.expect(ConfigurationHelper.getInstance()).andReturn(configurationHelper).anyTimes();
+        EasyMock.expect(configurationHelper.getMetsEditorLockingTime()).andReturn(1800000l).anyTimes();
+        EasyMock.expect(configurationHelper.isAllowWhitespacesInFolder()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.useS3()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.isUseProxy()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.getGoobiContentServerTimeOut()).andReturn(60000).anyTimes();
+        EasyMock.expect(configurationHelper.getMetadataFolder()).andReturn(metadataDirectoryName).anyTimes();
+        EasyMock.expect(configurationHelper.getRulesetFolder()).andReturn(resourcesFolder).anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImagesMainDirectoryName()).andReturn("folder_media").anyTimes();
+        EasyMock.expect(configurationHelper.isUseMasterDirectory()).andReturn(true).anyTimes();
+
+        EasyMock.expect(configurationHelper.getNumberOfMetaBackups()).andReturn(0).anyTimes();
+        EasyMock.replay(configurationHelper);
+
+        PowerMock.mockStatic(VariableReplacer.class);
+        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("folder_media").anyTimes();
+        PowerMock.replay(VariableReplacer.class);
+        prefs = new Prefs();
+        prefs.loadPrefs(resourcesFolder + "ruleset.xml");
+        Fileformat ff = new MetsMods(prefs);
+        ff.read(metaTarget.toString());
+
+        PowerMock.mockStatic(ConfigPlugins.class);
+        EasyMock.expect(ConfigPlugins.getProjectAndStepConfig(EasyMock.anyString(), EasyMock.anyObject(Step.class)))
+        .andReturn(getConfig())
+        .anyTimes();
+        PowerMock.replay(ConfigPlugins.class);
+
+        PowerMock.mockStatic(MetadatenHelper.class);
+        EasyMock.expect(MetadatenHelper.getMetaFileType(EasyMock.anyString())).andReturn("mets").anyTimes();
+        EasyMock.expect(MetadatenHelper.getFileformatByName(EasyMock.anyString(), EasyMock.anyObject())).andReturn(ff).anyTimes();
+        EasyMock.expect(MetadatenHelper.getMetadataOfFileformat(EasyMock.anyObject(), EasyMock.anyBoolean()))
+        .andReturn(Collections.emptyMap())
+        .anyTimes();
+        PowerMock.replay(MetadatenHelper.class);
+
+        PowerMock.mockStatic(MetadataManager.class);
+        MetadataManager.updateMetadata(1, Collections.emptyMap());
+        MetadataManager.updateJSONMetadata(1, Collections.emptyMap());
+        PowerMock.replay(MetadataManager.class);
+        PowerMock.replay(ConfigurationHelper.class);
+
+        process = getProcess();
+
+        Ruleset ruleset = PowerMock.createMock(Ruleset.class);
+        ruleset.setTitel("ruleset");
+        ruleset.setDatei("ruleset.xml");
+        EasyMock.expect(ruleset.getDatei()).andReturn("ruleset.xml").anyTimes();
+        process.setRegelsatz(ruleset);
+        EasyMock.expect(ruleset.getPreferences()).andReturn(prefs).anyTimes();
+        PowerMock.replay(ruleset);
+    }
+
+    public Process getProcess() {
+        Project project = new Project();
+        project.setTitel("SampleProject");
+
+        Process process = new Process();
+        process.setTitel("Sample");
+        process.setProjekt(project);
+        process.setId(1);
+        List<Step> steps = new ArrayList<>();
+        Step s1 = new Step();
+        s1.setReihenfolge(1);
+        s1.setProzess(process);
+        s1.setTitel("test step");
+        s1.setBearbeitungsstatusEnum(StepStatus.OPEN);
+        User user = new User();
+        user.setVorname("Firstname");
+        user.setNachname("Lastname");
+        s1.setBearbeitungsbenutzer(user);
+        steps.add(s1);
+
+        process.setSchritte(steps);
+
+        try {
+            createProcessDirectory(processDirectory);
+        } catch (IOException e) {
+        }
+
+        return process;
+    }
+
+    private void createProcessDirectory(File processDirectory) throws IOException {
+        // image folder
+        File imageDirectory = new File(processDirectory.getAbsolutePath(), "images");
+        imageDirectory.mkdir();
+        // master folder
+        File masterDirectory = new File(imageDirectory.getAbsolutePath(), "folder_master");
+        masterDirectory.mkdir();
+
+        // media folder
+        File mediaDirectory = new File(imageDirectory.getAbsolutePath(), "folder_media");
+        mediaDirectory.mkdir();
+    }
+
+    private SubnodeConfiguration getConfig() {
+        String file = "plugin_intranda_step_folderimport.xml";
+        XMLConfiguration config = new XMLConfiguration();
+        config.setDelimiterParsingDisabled(true);
+        try {
+            config.load(file);
+        } catch (ConfigurationException e) {
+        }
+        config.setReloadingStrategy(new FileChangedReloadingStrategy());
+        return config.configurationAt("config");
+    }
+
 }

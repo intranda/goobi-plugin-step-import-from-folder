0be214c4322a3d2bb056583918ef04a288f4de8a
Robert Sehr <robert.sehr@intranda.com>
2021-11-22 08:03:16 +0100

    renamed plugin

--- plugin/src/main/java/de/intranda/goobi/plugins/FolderimportStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/FolderimportStepPlugin.java
@@ -36,10 +36,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleStepPlugin implements IStepPluginVersion2 {
+public class FolderimportStepPlugin implements IStepPluginVersion2 {
     
     @Getter
-    private String title = "intranda_step_sample";
+    private String title = "intranda_step_folderimport";
     @Getter
     private Step step;
     @Getter
@@ -57,7 +57,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
         value = myconfig.getString("value", "default value"); 
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("Sample step plugin initialized");
+        log.info("Folderimport step plugin initialized");
     }
 
     @Override
@@ -70,7 +70,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sample.xhtml";
+        return "/uii/plugin_step_folderimport.xhtml";
     }
 
     @Override
@@ -109,7 +109,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         boolean successfull = true;
         // your logic goes here
         
-        log.info("Sample step plugin executed");
+        log.info("Folderimport step plugin executed");
         if (!successfull) {
             return PluginReturnValue.ERROR;
         }
